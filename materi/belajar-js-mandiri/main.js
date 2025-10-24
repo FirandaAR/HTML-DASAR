95c55cd3e00288874b74c3b95d77a95333aec536
FirandaAR <abdurrozzaq47@gmail.com>
2025-10-24 16:22:25 +0700

    tugas components

---
+++ materi/belajar-js-mandiri/main.js
@@ -0,0 +1,48 @@
+// console.log('heyoo')
+
+// const x = 1
+// let nama = 'Firanda AbdurRozzaq'
+// let tb = 176.5
+// let usia = 20
+// let isMarried = false
+// if (isMarried === false) {
+//   console.log('anda belum menikah')
+// }
+// console.log('heyy umur mu adalah ' + usia)
+// alert('usia anda adalah' + usia)
+
+// // filepath:
+// // ...existing code...
+// // let usia = prompt('berapa usia kamu?')
+// // ...existing code...
+// // alert(`umur saya ${usia}`)
+// // ...existing code...
+
+// let saldoawal = 1000000
+// let saldotambahan = 80000
+// const saldoakhir = saldoawal + saldotambahan
+// alert(
+//   `nama saya adalah ${nama},tinggi badan saya adalah ${tb} cm, dan usia saya adalah ${usia} tahun`
+// )
+// alert(
+//   `saldo awal saya adalah ${saldoawal}, setelah saya menabung sebesar ${saldotambahan}, maka saldo akhir saya adalah ${saldoakhir}`
+// )
+
+// array
+let buah = ['mangga', 'apel', 'jeruk', 'kiwi']
+console.log(buah[0]) //mangga
+console.log(buah[1]) //apel
+console.log(buah[2]) //jeruk
+buah.push('semangka')
+console.log(buah) //['mangga','apel','jeruk','kiwi','semangka']
+
+// for loop
+
+// 3 statement
+console.log(buah.length) //5
+
+
+for (let i = 0; i <= buah.length; i++) {
+  console.log(buah[i]   ) 
+}
+ 
